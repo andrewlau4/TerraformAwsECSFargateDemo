fb02793728248f6ac606a111965bdc641f6df1ff
Andrew Lau <andrewlau4@hotmail.com>
2023-06-16 16:03:47 -0400

    added more disable / enable options

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "az_to_deploy_autoscale_capacity_provider" {
 output "subnet_ids_to_deploy_autoscale_capacity_provider" {
   value =  local.service_deploy_to_subnet_ids
 }
+
+output "fargate_service_name" {
+  value =  module.ecs_service_and_task.fargate_service_name
+}
+
+output "ec2_service_name" {
+  value =  module.ecs_service_and_task.ec2_service_name
+}
