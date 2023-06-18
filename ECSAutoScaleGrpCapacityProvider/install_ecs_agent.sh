56a4fceada89d24d1024f8bc1e18c94af9be2963
Andrew Lau <andrewlau4@hotmail.com>
2023-06-18 12:47:28 -0400

    fixing ec2

--- ECSAutoScaleGrpCapacityProvider/install_ecs_agent.sh
+++ ECSAutoScaleGrpCapacityProvider/install_ecs_agent.sh
@@ -7,10 +7,17 @@
 # curl -s http://localhost:51678/v1/metadata | python -mjson.tool
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_container_instance.html
+
+# see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
+#  if you start this file with #!, you don't need to use  sudo
+#    Scripts entered as user data are run as the root user, so do not use the sudo command in the script
 echo ECS_CLUSTER=${ecs_cluster_name} >> /etc/ecs/ecs.config
 
-sudo yum install -y ecs-init
-sudo service docker start
-sudo service ecs start
+#sudo yum install -y ecs-init
+#sudo service docker start
+#sudo service ecs start
+yum install -y ecs-init
+service docker start
+service ecs start
 curl -s http://localhost:51678/v1/metadata | python3 -mjson.tool
 # yum install -y aws-cli
